793d420e0febf93d43a5c917286dddfb6a074622
Danilo Morales <danilo.morales@perficient.com>
2024-10-02 23:21:29 -0500

    fix footer

--- src/app/_components/footer/footer.tsx
+++ src/app/_components/footer/footer.tsx
@@ -11,6 +11,10 @@ export const Footer = () => {
     },
     {
       id: uuid(),
+      value: "Cra 49 # 52 - 141, local 231",
+    },
+    {
+      id: uuid(),
       value: "inmobiliariadonquijote@gmail.com",
     },
   ];
@@ -19,18 +23,22 @@ export const Footer = () => {
     {
       id: uuid(),
       value: "Inmobiliaria",
+      link: "https://www.inmobiliariadonquijote.com/",
     },
     {
       id: uuid(),
       value: "Abogados",
+      link: "/derecho",
     },
     {
       id: uuid(),
       value: "Seguros",
+      link: "/seguros",
     },
     {
       id: uuid(),
       value: "Viajes",
+      link: "/viajes",
     },
   ];
 
@@ -38,6 +46,7 @@ export const Footer = () => {
     {
       id: uuid(),
       value: "¿Quiénes somos?",
+      link: "https://www.inmobiliariadonquijote.com/nosotros",
     },
     {
       id: uuid(),
@@ -45,48 +54,7 @@ export const Footer = () => {
     },
   ];
 
-  const places: ItemProp[] = [
-    {
-      id: uuid(),
-      value: "Medellín",
-    },
-    {
-      id: uuid(),
-      value: "Envigado",
-    },
-    {
-      id: uuid(),
-      value: "Sabaneta",
-    },
-    {
-      id: uuid(),
-      value: "Bello",
-    },
-    {
-      id: uuid(),
-      value: "Guatapé",
-    },
-    {
-      id: uuid(),
-      value: "Rionegro",
-    },
-    {
-      id: uuid(),
-      value: "Elretiro",
-    },
-    {
-      id: uuid(),
-      value: "El carmen de viboral",
-    },
-    {
-      id: uuid(),
-      value: "San Antonio de Pereira",
-    },
-    {
-      id: uuid(),
-      value: "Marinilla",
-    },
-  ];
+  
   return (
     <footer className="border-t-2 mt-auto">
       <div className="border-b-2 py-6">
@@ -103,11 +71,11 @@ export const Footer = () => {
       <div className="flex flex-col md:flex-row md:justify-between my-8  flex-wrap mx-12">
         <ItemFooter title="Contácto" items={contacto} />
         <ItemFooter title="Sobre Nosotros" items={aboutUs} />
-        <ItemFooter title="Propiedades" items={places} columns={3} />
+        <ItemFooter title="Propiedades" items={[]} columns={[].length < 4 ? 1 : 3} />
         <ItemFooter title="Servicios" items={servicios} />
       </div>
       <div className="bg-custom-orange block  md:flex md:justify-end  md:items-center py-2 md:mx-12 ">
-        <Link href="">
+        <Link href="https://www.instagram.com/inmobiliariadonquijote">
           <div className="flex my-2 md:my-0 mx-12 md:mx-0">
             <Image
               src="/logotipo-de-instagram.png"
@@ -119,13 +87,8 @@ export const Footer = () => {
           </div>
         </Link>
         <Link href="">
-          <div className="flex my-2 md:my-0 mx-12 md:mx-0"> 
-            <Image
-              src="/facebook.png"
-              width={22}
-              height={12}
-              alt="intagram"
-            />
+          <div className="flex my-2 md:my-0 mx-12 md:mx-0">
+            <Image src="/facebook.png" width={22} height={12} alt="intagram" />
             <p className="text-light mx-3">Inmobiliariadonquijote</p>
           </div>
         </Link>
